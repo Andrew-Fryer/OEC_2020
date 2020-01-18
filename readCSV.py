43afde16c4d8f0d5693bf3916049cdc86a798fcc
Andrew Fryer <andrew.fryer@queensu.ca>
2020-01-18 06:36:40 +0000

    comments and ordering

--- readCSV.py
+++ readCSV.py
@@ -3,21 +3,29 @@ import Input
 import Output
 import GreedyAlgorithm
 
+# read in csv file
 data = pd.read_csv("./given-files/inputFile1.csv", header=None)
+# replace blanks with zeros
 data = data.fillna(0)
 
+# the first 3 lines are the starting conditions
 starting_conditions = list(map(Output.Output, data.values[:3]))
-
+# the rest of the lines are inputs
 inputs = list(map(Input.Input, data.values[3:-1]))
 
+# Generate outputs
 outputs = []
-prev_nuclear = data.values.tolist()[2][4]
+prev_nuclear = data.values.tolist()[2][4] # nuclear supply form last line of starting conditions
 for my_input in inputs:
-    ordering_from_weighting = ["hydro", "wind", "biofuel", "nuclear", "neighbor", "solar", "gas"]
-    ordering = ["nuclear", "hydro", "wind", "biofuel", "neighbor", "solar", "gas"]
-    ordering_ontario_current = ["nuclear", "hydro", "gas", "wind", "solar", "biofuel", "neighbor"]
+    # Here, we only look at current and past data
+    # Seriously, we aren't cheating at all!
 
-    output = GreedyAlgorithm.GreedyAlgorithm(my_input, ordering_from_weighting, prev_nuclear)
+    #ordering_from_weighting = ["hydro", "wind", "biofuel", "nuclear", "neighbor", "solar", "gas"]
+    # this is the ordering_from_weighting, but with nuclear first
+    ordering = ["nuclear", "hydro", "wind", "biofuel", "neighbor", "solar", "gas"]
+    #ordering_ontario_current = ["nuclear", "hydro", "gas", "wind", "solar", "biofuel", "neighbor"]
+
+    output = GreedyAlgorithm.GreedyAlgorithm(my_input, ordering, prev_nuclear)
     outputs.append(output.to_array(my_input))
 
     prev_nuclear = output.sources["nuclear"]
