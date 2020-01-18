086a9f2c512e568f119fd310b5751f6af2c5c87e
Andrew Fryer <andrew.fryer@queensu.ca>
2020-01-18 04:43:53 +0000

    fix nuclear

--- GreedyAlgorithm.py
+++ GreedyAlgorithm.py
@@ -1,7 +1,19 @@
 import Output
-def GreedyAlgorithm(input, ordering): # ordering is just a list of strings... :)
+import math
+
+def clamp(n):
+    return max(min(n, 0.01), -0.01)
+def GreedyAlgorithm(input, ordering, prev_nuclear): # ordering is just a list of strings... :)
     demand = input.demands
     output = Output.Output(None)
+
+    # Farleyness
+    nuclear_ratio = (demand * 1.025 - (float(input.sources["wind"]) + float(input.sources["hydro"]) + float(input.sources["biofuel"]))) / prev_nuclear
+    history_ratio = abs(input.last_week[1] / input.last_week[0] - 1)
+    nuclear_change = clamp(history_ratio * (nuclear_ratio-1) / abs(nuclear_ratio - 1))
+    input.sources["nuclear"] = prev_nuclear * (nuclear_change + 1)
+    if math.isnan(input.sources["nuclear"]):
+        print("yo")
 
     for i in range(len(ordering)):
         supply = input.sources[ordering[i]]
--- readCSV.py
+++ readCSV.py
@@ -4,18 +4,22 @@ import Output
 import GreedyAlgorithm
 
 data = pd.read_csv("./given-files/inputFile1.csv", header=None)
+data = data.fillna(0)
 
-starting_conditions = map(Output.Output, data.values[:3])
+starting_conditions = list(map(Output.Output, data.values[:3]))
 
-inputs = map(Input.Input, data.values[3:-1])
+inputs = list(map(Input.Input, data.values[3:-1]))
 
 outputs = []
+prev_nuclear = starting_conditions[-1].sources["nuclear"]
 for my_input in inputs:
     ordering_from_weighting = ["hydro", "wind", "biofuel", "nuclear", "neighbor", "solar", "gas"]
     ordering = ["nuclear", "hydro", "wind", "biofuel", "neighbor", "solar", "gas"]
     ordering_ontario_current = ["nuclear", "hydro", "gas", "wind", "solar", "biofuel", "neighbor"]
-    my_input.sources["nuclear"] = 10781
-    output = GreedyAlgorithm.GreedyAlgorithm(my_input, ordering_from_weighting)
+
+    output = GreedyAlgorithm.GreedyAlgorithm(my_input, ordering_from_weighting, prev_nuclear)
     outputs.append(output.to_array())
+
+    prev_nuclear = output.sources["nuclear"]
 
 print(outputs)
